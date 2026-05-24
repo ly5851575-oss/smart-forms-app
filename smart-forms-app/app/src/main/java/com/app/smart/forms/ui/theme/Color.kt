363a88e2ed100ec0779c8906bece559705717f7d
ly5851575-oss <ly5851575@gmail.com>
2026-05-24 05:46:56 +0300

    Order 1: Add Compose color palette

---
+++ smart-forms-app/app/src/main/java/com/app/smart/forms/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.app.smart.forms.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val SmartPrimary = Color(0xFF2563EB)
+val SmartPrimaryDark = Color(0xFF93C5FD)
+val SmartSecondary = Color(0xFF0F172A)
+val SmartBackground = Color(0xFFF8FAFC)
